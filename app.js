9121ae0a6619d72d87a0f64ef8ff8ab7be49ed79
Theo02-12 <mrcouvelard@gmail.com>
2024-03-11 16:53:46 +0100

    build: dark mode is working

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const navbar = document.getElementById("navbar");
 
 function navBarState() {
-    if (navbar.style.display === "none") {
-      navbar.style.display = "block";
-    } else {
-      navbar.style.display = "none";
-    }
-  
+  if (navbar.style.display === "none") {
+    navbar.style.display = "block";
+  } else {
+    navbar.style.display = "none";
+  }
+}
+
+function setDarkMode() {
+  document.querySelector("body").classList.add("dark");
+  document.getElementById("theme-icon").src = "/icons/sun.svg";
+
+  if (localStorage.getItem("theme") === "dark") {
+    document.querySelector("body").classList.remove("dark");
+    document.getElementById("theme-icon").src = "/icons/moon.svg";
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
 }
