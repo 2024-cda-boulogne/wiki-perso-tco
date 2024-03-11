3ac70d4e268fefa9834ab7d3e522b2acc13ab60b
Theo02-12 <mrcouvelard@gmail.com>
2024-03-11 15:17:11 +0100

    build: added navbar with icons and responsive

---
+++ app.js
@@ -0,0 +1,10 @@
+const navbar = document.getElementById("navbar");
+
+function navBarState() {
+    if (navbar.style.display === "none") {
+      navbar.style.display = "block";
+    } else {
+      navbar.style.display = "none";
+    }
+  
+}
